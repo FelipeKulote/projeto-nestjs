b2d4a8514b627ee0911f8cf9273f471035abe074
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-12-06 10:36:27 -0300

    feat: implement swagger

--- src/department/department.controller.ts
+++ src/department/department.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { DepartmentService } from './department.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('departments')
 @Controller('department')
 export class DepartmentController {
   constructor(private departmentService: DepartmentService) {}
