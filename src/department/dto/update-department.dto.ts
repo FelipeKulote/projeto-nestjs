8dbaefc1e299df74837c6ba0d5069c3baaa0f254
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-12-08 15:38:15 -0300

    style: prettier format

--- src/department/dto/update-department.dto.ts
+++ src/department/dto/update-department.dto.ts
@@ -1,6 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDepartmentDto } from './create-department.dto';
 
-export class UpdateDepartment extends PartialType(CreateDepartmentDto) {
-    
-}
+export class UpdateDepartment extends PartialType(CreateDepartmentDto) {}
