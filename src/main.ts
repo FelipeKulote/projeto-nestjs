790f0c65bbe48293c45083b3afe589a688acffcf
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-12-08 16:04:51 -0300

    feat (product): implement controller

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .setVersion('1.0.0')
     .addTag('status')
     .addTag('departments')
+    .addTag('product')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
