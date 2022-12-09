0c736341e0943534e674eac53e1b286201275348
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-12-09 16:29:04 -0300

    feat (user): implement entity and dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,56 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl, Matches } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do usuário',
+    example: 'Felipe Augusto',
+  })
+  name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'E-mail do usuário',
+    example: 'exemplo@mail.com',
+  })
+  email: string;
+
+  @IsString()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca',
+  })
+  @ApiProperty({
+    description: 'Senha para login',
+    example: '#Senhasegura123',
+  })
+  password: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Confirmação de senha para criação do usuário',
+    example: '#Senhasegura123',
+  })
+  confirmPassword: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'CPF do usuário',
+    example: '999.999.999-99',
+  })
+  cpf: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem de perfil do usuário',
+    example: 'https://avatars.githubusercontent.com/u/106357116',
+  })
+  image: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Nivel de acesso do sistema',
+    example: 'Administrador',
+  })
+  role: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+export class User {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  cpf: string;
+  image: string;
+  role: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
