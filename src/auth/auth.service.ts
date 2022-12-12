b35111eebb38d74dab8f146c7b05d58ac3c3d23a
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-12-12 15:01:03 -0300

    feat (auth): implement login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
@@ -6,7 +7,10 @@ import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
     // const { email, password } = loginDto;
@@ -27,7 +31,7 @@ export class AuthService {
     delete user.password;
 
     return {
-      token: 'Teste',
+      token: this.jwtService.sign({ email }),
       user,
     };
   }
--- src/auth/dto/login-response.dto.ts
+++ src/auth/dto/login-response.dto.ts
@@ -4,7 +4,8 @@ import { User } from 'src/user/entities/user.entity';
 export class LoginResponseDto {
   @ApiProperty({
     description: 'JWT gerado pelo login',
-    example: 'TOKEN_GERADO_AUTOMATICAMENTE',
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV4ZW1wbG9AbWFpbC5jb20iLCJpYXQiOjE2NzA4Njc5NzMsImV4cCI6MTY3MDk1NDM3M30.d1INRnk4uTIu9wnZQIlt77O5-Sdz7LE6cW38xrV6w1Y',
   })
   token: string;
 
